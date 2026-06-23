e626b4aba8456f211019538842da39d5aaa61c6f
Abalbuena00 <balbuena.alexb@gmail.com>
2026-06-23 14:26:25 -0400

    feat(persistence): Crean la estructura base de persistencia

---
+++ SIGEBI.Persistence/Constants/SchemaNames.cs
@@ -0,0 +1,12 @@
+﻿namespace SIGEBI.Persistence.Constants;
+
+public static class SchemaNames
+{
+    public const string Seguridad = "seguridad";
+    public const string Catalogo = "catalogo";
+    public const string Prestamos = "prestamos";
+    public const string Penalizaciones = "penalizaciones";
+    public const string Notificaciones = "notificaciones";
+    public const string Auditoria = "auditoria";
+    public const string Configuracion = "configuracion";
+}
